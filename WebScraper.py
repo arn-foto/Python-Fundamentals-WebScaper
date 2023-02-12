a2c6dfd8552f6ec0cd867a0591ae6cc471e49f2c
Anthony <foto.arn@gmail.com>
2023-02-12 14:49:24 -0800

    Area row added

--- WebScraper.py
+++ WebScraper.py
@@ -17,23 +17,25 @@ file = open('GatheredInfo.csv', 'w')
 # In this case, we're saving it to our csv file with a function attached to it.
 writer = csv.writer(file)
 
-writer.writerow(['Name', 'Capital'])
+writer.writerow(['Name', 'Capital', 'Population', 'Area'])
 
 # This is is where we are calling our import and sends a GET request to the specified url
 target_page = requests.get("http://www.scrapethissite.com/pages/simple/")
 
 # BeautifulSoup() is built into bs4 and takes 2 arguments, one for the target and the other for the format
-soup = BeautifulSoup(target_page.text, 'lxml')
+soup = BeautifulSoup(target_page.text, 'html.parser')
 
 # We are taking our assigned variables and going through the CSS file to grab from the tags we chose. 
 name = soup.find_all('h3', attrs={"class":"country-name"})
 capital = soup.find_all('span', attrs={"class":"country-capital"})
+population = soup.find_all('span', attrs={"class":"country-population"})
+area = soup.find_all('span', attrs={"class":"country-area"})
 
 # The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, 
 # and then the second item in each passed iterator are paired together etc.
-for name, capital in zip(name, capital):
-    print(name.text + "-" + capital.text)
+for name, capital, population, area in zip(name, capital, population, area):
+    print(name.text + "-" + capital.text + "-" + population.text + "-" + area.text)
    
-    writer.writerow([name.text, capital.text])
+    writer.writerow([name.text, capital.text, population.text, area.text])
 # close() does what you think it does.
 file.close()
