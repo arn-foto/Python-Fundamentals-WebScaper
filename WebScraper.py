68bd85ea9f1c1b7e9e056b65cec13f90ae26ce65
Anthony <foto.arn@gmail.com>
2023-02-12 14:12:58 -0800

    Reformat for countries instead of quotes

--- WebScraper.py
+++ WebScraper.py
@@ -17,23 +17,23 @@ file = open('GatheredInfo.csv', 'w')
 # In this case, we're saving it to our csv file with a function attached to it.
 writer = csv.writer(file)
 
-writer.writerow(['Quote', 'Author'])
+writer.writerow(['Name', 'Info'])
 
 # This is is where we are calling our import and sends a GET request to the specified url
-target_page = requests.get("http://quotes.toscrape.com")
+target_page = requests.get("http://www.scrapethissite.com/pages/simple/")
 
 # BeautifulSoup() is built into bs4 and takes 2 arguments, one for the target and the other for the format
 soup = BeautifulSoup(target_page.text, 'lxml')
 
 # We are taking our assigned variables and going through the CSS file to grab from the tags we chose. 
-quotes = soup.find_all('span', attrs={"class":"text"})
-authors = soup.find_all('small', attrs={"class":"author"})
+name = soup.find_all('h3', attrs={"class":"country-name"})
+info = soup.find_all('div', attrs={"class":"country-info"})
 
 # The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, 
 # and then the second item in each passed iterator are paired together etc.
-for quote, author in zip(quotes, authors):
-    print(quote.text + "-" + author.text)
+for name, info in zip(name, info):
+    print(name.text + "-" + info.text)
    
-    writer.writerow([quote.text, author.text])
-# close() does what you think it does. 
+    writer.writerow([name.text, info.text])
+# close() does what you think it does.
 file.close()
