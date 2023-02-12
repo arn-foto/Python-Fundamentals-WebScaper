1a1eb5a6a69a1277430f33bd069e808defe65aaa
Anthony <foto.arn@gmail.com>
2023-02-12 03:09:48 -0800

    Add comment for import

--- WebScraper.py
+++ WebScraper.py
@@ -1,5 +1,8 @@
+#BeautifulSoup is a used to pull out data from HTML and XML files.
 from bs4 import BeautifulSoup
+#This import makes a get request to a web page.
 import requests
+# Creates a csv file with the information pulled from requests.
 import csv
 
 print("this works")
